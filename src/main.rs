f8364b13d671ff1fb2f8ce5397cc65271387dd19
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2026-05-20 16:22:18 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ fn main() {
 
 enum Command {
     Exit,
+    Echo(String),
     Unknown,
 }
 
 fn commands_from_string(command: &str) -> Command {
-    match command {
+    let args = command.split(" ").collect::<Vec<&str>>();
+    match args[0] {
         "exit" => Command::Exit,
+        "echo" => Command::Echo(args[1..].join(" ")),
         _ => Command::Unknown,
     }
 }
@@ -30,6 +33,7 @@ fn repl() {
             let cmd = commands_from_string(command);
             match cmd {
                 Command::Exit => break,
+                Command::Echo(text) => println!("{}", text),
                 Command::Unknown => println!("{}: command not found", command),
             }
         }
