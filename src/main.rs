c6ac3e94580aa4bd08cd82c76518990669bcaa06
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2026-05-20 12:12:42 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,18 @@ fn main() {
     repl();
 }
 
+enum Command {
+    Exit,
+    Unknown,
+}
+
+fn commands_from_string(command: &str) -> Command {
+    match command {
+        "exit" => Command::Exit,
+        _ => Command::Unknown,
+    }
+}
+
 fn repl() {
     loop {
         print!("$ ");
@@ -12,9 +24,14 @@ fn repl() {
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+        let command = command.trim();
 
-        if command.trim() != "" {
-            println!("{}: command not found", command.trim());
+        if command != "" {
+            let cmd = commands_from_string(command);
+            match cmd {
+                Command::Exit => break,
+                Command::Unknown => println!("{}: command not found", command),
+            }
         }
     }
 }
