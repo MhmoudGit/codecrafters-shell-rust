8247de1765f63e965fba1b20bd149e44ac75c0d5
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2026-05-20 16:38:21 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,31 @@ fn main() {
 
 enum Command {
     Exit,
-    Echo(String),
+    Echo,
+    Type,
     Unknown,
 }
 
 fn commands_from_string(command: &str) -> Command {
-    let args = command.split(" ").collect::<Vec<&str>>();
-    match args[0] {
+    match command {
         "exit" => Command::Exit,
-        "echo" => Command::Echo(args[1..].join(" ")),
+        "echo" => Command::Echo,
+        "type" => Command::Type,
         _ => Command::Unknown,
+    }
+}
+
+fn echo_command(text: Vec<&str>) {
+    println!("{}", text.join(" "));
+}
+
+fn type_command(command: Vec<&str>) {
+    let cmd_name = command[0];
+    let cmd = commands_from_string(&cmd_name);
+    if let Command::Unknown = cmd {
+        println!("{}: command not found", cmd_name)
+    } else {
+        println!("{}: is a shell builtin", cmd_name)
     }
 }
 
@@ -30,10 +45,15 @@ fn repl() {
         let command = command.trim();
 
         if command != "" {
-            let cmd = commands_from_string(command);
+            let args = command.split(" ").collect::<Vec<&str>>();
+            let command_name = args[0];
+            let command_args = args[1..].to_vec();
+
+            let cmd = commands_from_string(command_name);
             match cmd {
                 Command::Exit => break,
-                Command::Echo(text) => println!("{}", text),
+                Command::Echo => echo_command(command_args),
+                Command::Type => type_command(command_args),
                 Command::Unknown => println!("{}: command not found", command),
             }
         }
